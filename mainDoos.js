e60dcaf8ce4ef6323463be98ca7ef122e205bf29
EqueTheSlayer <o_orlandina@mail.ru>
2020-04-08 15:38:59 +0400

    fixing music functions

--- mainDoos.js
+++ mainDoos.js
@@ -92,8 +92,6 @@ bot.on('message', async msg => {
         }
 
     }
-    queue.set(msg.guild.id, queueContruct);
-    queueContruct.songs.push(song);
     function play(guild, song) {
         const serverQueue = queue.get(guild.id);
         if (!song) {
