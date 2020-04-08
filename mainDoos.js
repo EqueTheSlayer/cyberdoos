ac152c17247a8b2e4d476a5fc1563a697452573f
EqueTheSlayer <o_orlandina@mail.ru>
2020-04-08 15:34:49 +0400

    fixing music functions

--- mainDoos.js
+++ mainDoos.js
@@ -94,14 +94,12 @@ bot.on('message', async msg => {
     }
     queue.set(msg.guild.id, queueContruct);
     queueContruct.songs.push(song);
-    if (msg.content.includes(`${prefix}play`)) {
-        function play(guild, song) {
-            const serverQueue = queue.get(guild.id);
-            if (!song) {
-                serverQueue.voiceChannel.leave();
-                queue.delete(guild.id);
-                return;
-            }
+    function play(guild, song) {
+        const serverQueue = queue.get(guild.id);
+        if (!song) {
+            serverQueue.voiceChannel.leave();
+            queue.delete(guild.id);
+            return;
         }
     }
     const dispatcher = serverQueue.connection.playStream(ytdl(song.url, { filter: "audioonly" }))
