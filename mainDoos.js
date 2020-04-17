61396056e17e4cb3df64db76dd2b71cd69fa6da9
EqueTheSlayer <o_orlandina@mail.ru>
2020-04-17 12:50:26 +0400

    adding delete message after amount of time

--- mainDoos.js
+++ mainDoos.js
@@ -42,7 +42,7 @@ bot.on('ready', () => {
 });
 
 bot.on('message', async msg => {
-    setTimeout(msg.delete(), 3000);
+    let deleting = setTimeout(msg.author.bot.delete(), 3000);
     if (msg.content.startsWith(`/play`)) {
         msg.channel.send(`\`\`\`А как же я?😥😥😥\`\`\``)
     }
