61e515ab38f2ecb156b09fd7be1d7c0ce69057fc
EqueTheSlayer <o_orlandina@mail.ru>
2020-04-20 15:36:57 +0400

    checking for songLink

--- mainDoos.js
+++ mainDoos.js
@@ -205,7 +205,7 @@ bot.on('message', async msg => {
             if (msg.content.startsWith('!play http')) {
                 songLink2 = args[0];
                 console.log(songLink2);
-                if (songLink2.search('/^[a-zA-Z0-9-_]{11}$/') = -1) {
+                if (songLink2.search('/^[a-zA-Z0-9-_]{11}$/') === -1) {
                     msg.channel.send(
                         {
                             embed: {
