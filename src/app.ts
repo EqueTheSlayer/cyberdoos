d1d4ec71bdae7ed0b33733a43d9f45cdd34772ab
Anton Tarasov <atarasov@emphasoft.com>
2021-04-26 12:14:28 +0300

    Fixed import paths.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
-import {CyberDoos} from "CyberDoos/CyberDoos";
+import {addPath} from "app-module-path";
+addPath(__dirname);
 import config from "botconfig.json";
 import {CoinFlipCommand} from "commands/CoinFlip.command";
+import {CyberDoos} from "CyberDoos/CyberDoos";
 
 const commands = [CoinFlipCommand];
 const cyberDoos = new CyberDoos(config, commands);
