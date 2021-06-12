53906e9fa4195ccd9585e50f1ad4e82642629af5
ilyaorlov98 <io@intervolga.ru>
2021-06-12 23:04:36 +0300

    добавил бета-версию очереди песен

--- src/commands/MusicCommand.ts
+++ src/commands/MusicCommand.ts
@@ -5,58 +5,110 @@ import ytdl from "ytdl-core";
 import config from "../botconfig.json";
 import youtubeSearch, {YouTubeSearchResults} from "youtube-search";
 import {timeout} from "../CyberDoos/CyberDoos.model";
+import {VoiceConnection} from "discord.js";
 
 export class MusicCommand implements CommandBase {
-    commandName = ["play", "stop", "next", "queue"];
-    play: Play = {
-        connection: null,
-        dispatcher: null,
-    };
-    searchOptions: youtubeSearch.YouTubeSearchOptions = {
-        maxResults: 1,
-        key: config.youtubeToken,
-        order: 'relevance'
-    }
+  commandName = ["play", "stop", "next", "queue"];
 
-    do(command: string, args: string[], message: Message) {
-        switch (command) {
-            case 'play':
-                return this.playStream(message, args);
-            case 'stop':
-                return this.stopStream(message);
-        }
-    }
+  play: Play = {
+    connection: null,
+    dispatcher: null,
+  };
 
-    connect(message: Message) {
-        if (message.member.voice.channel) {
-            return message.member.voice.channel.join();
+  queue:Array<{songName: string, songLink: string}> = [];
+
+  searchOptions: youtubeSearch.YouTubeSearchOptions = {
+    maxResults: 1,
+    key: config.youtubeToken,
+    order: 'relevance'
+  }
+
+  do(command: string, args: string[], message: Message) {
+    switch (command) {
+      case 'play':
+        return this.playStream(message, args);
+      case 'stop':
+        return this.stopStream(message);
+      case 'next':
+        return this.nextSong(message);
+    }
+  }
+
+  connectToVoiceChannel(message: Message) {
+    if (message.member.voice.channel) return message.member.voice.channel.join();
+  }
+
+  playStream(message: Message, args: string[]): Promise<string> {
+    return new Promise(async (resolve, reject) => {
+      this.play.connection = await this.connectToVoiceChannel(message);
+      if (!this.play.connection) {
+        resolve('Сначала зайди на голосовой канал!');
+        return;
+      }
+
+
+      await youtubeSearch(args.join(' '), this.searchOptions, (err: Error, results: YouTubeSearchResults[] | undefined) => {
+
+        if (err) reject(err);
+
+        this.queue.push({
+          songName: results[0].title,
+          songLink: results[0].link
+        });
+
+        if (this.queue.length > 1) {
+          resolve(`${this.queue[0].songName} добавлена в очередь.`);
         } else {
-            let guy = 'Сначала зайди на какой-нибудь голосовой канал';
+          resolve(this.playSong(message));
         }
-    }
+      });
+    })
+  }
 
-    playStream(message:Message, args: string[]):Promise<string> {
-        return new Promise(async (resolve, reject) => {
-            this.play.connection = await this.connect(message);
-            await youtubeSearch(args.join(' '), this.searchOptions, (err: Error, results: YouTubeSearchResults[] | undefined) => {
-                if (err) reject(err);
-                this.play.dispatcher = this.play.connection.play(ytdl(results[0].link, {filter: "audioonly"}));
-                resolve(`Воспроизвожу ${results[0].title}`);
-            });
-        })
-    }
-
-    stopStream(message:Message) {
-        this.play.dispatcher.destroy();
+  finishSongHandler(message: Message) {
+    this.play.dispatcher.on('finish', () => {
+      if (this.queue.length > 1) {
+        this.queue.pop();
+        this.playSong(message);
+      } else {
+        this.queue = [];
         this.leaveChannel(message, timeout);
+      }
+    })
+  }
 
-        return 'Песню спел, а теперь сыбался';
+  playSong(message: Message) {
+    this.play.dispatcher = this.play.connection.play(ytdl(this.queue[0].songLink, {filter: "audioonly"}));
+    this.finishSongHandler(message);
+    return `Воспроизвожу ${this.queue[0].songName}`;
+  }
+
+  stopStream(message: Message) {
+    if (this.play.dispatcher) {
+      this.play.dispatcher.destroy();
+
+      this.leaveChannel(message, timeout);
+
+      return 'Песню спел, пора по койкам';
     }
 
-    leaveChannel(message:Message, timeout:number) {
-        setTimeout(() => {
-            message.member.voice.channel.leave();
-        }, timeout);
+    return 'А никаких песен и не играет';
+  }
+
+  nextSong(message: Message) {
+    if (this.queue.length > 1) {
+      this.queue.pop();
+
+      return this.playSong(message);
+    } else {
+      return 'В очереди нет песен';
     }
+  }
+
+  leaveChannel(message: Message, timeout: number) {
+    setTimeout(() => {
+      message.member.voice.channel.leave();
+    }, timeout);
+  }
 }
 
